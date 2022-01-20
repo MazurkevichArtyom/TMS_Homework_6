b117bc5cf7e3e98e2b80ce91207f9c822a3caaba
Artem Mazurkevich <artyom.mazurkevich@gmail.com>
2022-01-20 21:39:46 +0300

    third task

---
+++ HW6-3/HW6-3/Enums/Direction.swift
@@ -0,0 +1,15 @@
+//
+//  Direction.swift
+//  HW6-3
+//
+//  Created by Artem Mazurkevich on 17.01.2022.
+//
+
+import Foundation
+
+enum Direction {
+    case up
+    case down
+    case left
+    case right
+}
